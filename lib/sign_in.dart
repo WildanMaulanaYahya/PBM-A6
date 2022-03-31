faa387d6870e8d1a39d7bc368db7c1f5f09441df
aisyahnasria <aisyahsmr18@gmail.com>
2022-03-31 15:04:29 +0700

    menambahkan flex

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -26,95 +26,92 @@ class MyApp extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(150, 0, 0, 50),
-              child: Image.asset(
-                'images/login.png',
-              ),
+              child: Image.asset('images/login.png'),
             ),
-            Container(
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.only(topLeft: Radius.circular(35))),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 20, 270, 0),
-                    child: Text(
-                      "Masuk",
-                      style: TextStyle(
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 30),
-                    child: TextField(
-                      style:
-                          const TextStyle(fontFamily: "Poppins", fontSize: 15),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        hintText: 'Email',
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      style:
-                          const TextStyle(fontFamily: "Poppins", fontSize: 15),
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
+            Expanded(
+                flex: 1,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.only(topLeft: Radius.circular(35))),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 20, 270, 0),
+                        child: Text(
+                          "Masuk",
+                          style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
                           ),
-                          hintText: 'Password',
-                          suffixIcon: const Icon(Icons.visibility)),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(250, 20, 0, 20),
-                    child: Text("Lupa Password ?",
-                        style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontSize: 13,
-                            color: Color(0xff8F8F8F))),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-                    child: SizedBox(
-                      width: 350,
-                      height: 50,
-                      child: TextButton(
-                          style: TextButton.styleFrom(
-                              backgroundColor: const Color(0xff3A8C6E),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              )),
-                          onPressed: () {},
-                          child: const Text(
-                            "Sign In",
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 30),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontFamily: "Poppins", fontSize: 15),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
                             ),
-                          )),
-                    ),
+                            hintText: 'Email',
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontFamily: "Poppins", fontSize: 15),
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              hintText: 'Password',
+                              suffixIcon: const Icon(Icons.visibility)),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(250, 20, 0, 20),
+                        child: Text("Lupa Password ?",
+                            style: TextStyle(
+                                fontFamily: "Poppins",
+                                fontSize: 13,
+                                color: Color(0xff8F8F8F))),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                        child: SizedBox(
+                          width: 350,
+                          height: 50,
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                  backgroundColor: const Color(0xff3A8C6E),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  )),
+                              onPressed: () {},
+                              child: const Text(
+                                "Sign In",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              )),
+                        ),
+                      ),
+                      const Text("Or Sign Up Here",
+                          style: TextStyle(
+                              fontFamily: "Poppins",
+                              fontSize: 13,
+                              color: Color(0xff8F8F8F)))
+                    ],
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 107),
-                    child: Text("Or Sign Up Here",
-                        style: TextStyle(
-                            fontFamily: "Poppins",
-                            fontSize: 13,
-                            color: Color(0xff8F8F8F))),
-                  )
-                ],
-              ),
-            )
+                ))
           ],
         ),
       ),
