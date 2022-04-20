40f6937da66696724b8419943645f61c47e3b67f
aisyahnasria <80971014+aisyahnasria@users.noreply.github.com>
2022-04-20 17:02:02 +0700

    membuat halaman barang

---
+++ lib/barang.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Barang extends StatelessWidget {
+  const Barang({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
