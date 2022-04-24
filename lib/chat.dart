53dbf73c0642336ad11f007fc4de6b95240dc36b
aisyahnasria <80971014+aisyahnasria@users.noreply.github.com>
2022-04-24 14:41:48 +0700

    membuat page chat

---
+++ lib/chat.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({Key? key}) : super(key: key);
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: const Color(0xff3A8C6E),
+          body: SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(25),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(Icons.arrow_back_outlined)),
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset('images/biji4.png')),
+                        const SizedBox(
+                          width: 30,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              'Biji 1',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: 'Poppins',
+                              ),
+                            ),
+                            Text('Rp 50.000',
+                                style: TextStyle(
+                                    color: Color(0xff4F9E2A),
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                    color: const Color(0xffF2F1F1),
+                    height: MediaQuery.of(context).size.height * 0.7),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontFamily: "Poppins", fontSize: 15),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            contentPadding: const EdgeInsets.all(5),
+                            hintText: 'Tulis Pesan...',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
