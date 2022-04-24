8e23e6a49926b3da3929c2d55eaf3a0845759ffd
aisyahnasria <80971014+aisyahnasria@users.noreply.github.com>
2022-04-24 14:42:32 +0700

    mengubah menjadi class profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,12 +2,8 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Profile extends StatelessWidget {
+  const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
