ffafa48033dba18284dfe4bc9409af9140a902e4
aisyahnasria <80971014+aisyahnasria@users.noreply.github.com>
2022-04-25 18:22:06 +0700

    membuat halaman cart

---
+++ lib/cart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xff3A8C6E),
+      ),
+    );
+  }
+}
