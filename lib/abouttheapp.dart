b87f84dd4cb72b0b193a09190dee017b204508e8
aisyahnasria <80971014+aisyahnasria@users.noreply.github.com>
2022-05-10 17:45:30 +0700

    meperbaiki text

--- lib/abouttheapp.dart
+++ lib/abouttheapp.dart
@@ -50,25 +50,10 @@ class Abouttheapp extends StatelessWidget {
                       child: const Padding(
                         padding: EdgeInsets.all(50.0),
                         child: Text(
-                          '''Lorem ipsum dolor sit amet, 
-            consectetur adipiscing elit ut 
-            aliquam, purus sit amet 
-            luctus venenatis, lectus 
-            magna fringilla urna, porttitor 
-            rhoncus dolor purus non 
-            enim praesent elementum 
-            facilisis leo, vel fringilla est 
-            ullamcorper eget nulla facilisi 
-            etiam dignissim diam quis 
-            enim lobortis scelerisque 
-            fermentum dui faucibus in 
-            ornare quam viverra orci 
-            sagittis eu volutpat odio 
-            facilisis mauris sit amet 
-            massa''',
+                          '''Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis eu volutpat odio facilisis mauris sit amet massa''',
                           style: TextStyle(
                               fontFamily: "poppins",
-                              fontSize: 16,
+                              fontSize: 17,
                               fontWeight: FontWeight.w600),
                         ),
                       ),
